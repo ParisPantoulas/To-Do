8104e1a35f1385b48e6ddd99cb2fee39f15a18f2
ParisPantoulas <47350548+ParisPantoulas@users.noreply.github.com>
2023-12-14 14:59:17 +0200

    Creating app and Sidebar

---
+++ to-do/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <Sidebar />
+  <div class="home">
+
+  </div>
+  
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar.vue';
+
+export default {
+  name: 'HomeView',
+  components: { Sidebar }
+}
+</script>
